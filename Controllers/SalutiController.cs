204b308c4d8187a2e471e8177940e7afa01e1a13
AngeloDalo <angelo251158@gmail.com>
2022-05-07 13:53:35 +0200

    6.0

---
+++ Controllers/SalutiController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalutiWebApi2.Controllers
+{
+    [ApiController]
+    [Route("api/saluti")]
+    public class SalutiController
+    {
+        public string getSaluti(){
+            return "Saluti, sono la tua prima web api creata in c#";
+        }
+    }
+}
