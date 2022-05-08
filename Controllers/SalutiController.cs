87c71c090ef1815241e1ddc7034577f1c22ebc2b
AngeloDalo <angelo251158@gmail.com>
2022-05-08 09:32:30 +0200

    gestione errore

--- Controllers/SalutiController.cs
+++ Controllers/SalutiController.cs
@@ -11,9 +11,23 @@ namespace SalutiWebApi2.Controllers
     public class SalutiController
     {
         public string getSaluti(){
-            return "Saluti, sono la tua prima web api creata in c#";
+            return "\"Saluti, sono la tua prima web api creata in c#\"";
         }
         [HttpGet("{Nome}")]
-        public string getSaluti(String Nome) => string.Format("Saluti, {0} sono il tuo primo web service creato in C# 6.0", Nome);
+        // public string getSaluti(String Nome) => string.Format("\"Saluti, {0} sono il tuo primo web service creato in C# 6.0\"", Nome);
+        public string getSaluti(string Nome)  
+        {
+            try
+            {
+                if (Nome == "Marco")
+                    throw new Exception("\"Errore: L'utente Marco è disabilitato!\"");
+                else 
+                    return  string.Format("\"Saluti, {0} sono il tuo primo web service creato con C# 6.0\"", Nome);
+            }
+            catch (System.Exception ex)
+            {
+                return ex.Message;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseCors(options =>
+    options 
+        .WithOrigins("http://localhost:4200")
+        .WithMethods("POST", "PUT", "DELATE", "GET")
+        .AllowAnyHeader()
+);
+
 app.MapControllers();
 
 app.Run();
